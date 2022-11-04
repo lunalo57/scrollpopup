5124e88efc6d83337ba64e3e725638e646897fdc
lunalo57 <lunalo.dev@gmail.com>
2022-11-04 10:55:27 +0100

    finalisation 2

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// Créer un événement au scroll
+window.addEventListener('scroll', () => {
+    //Navbar
+    if(window.scrollY > 50){
+        navbar.style.height = "45px"
+    }else{
+        navbar.style.height = "90px"
+    }
+    // Image
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if(scrollValue > 0.33){
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = "translateX(0px)";
+    }
+    // popup
+    if(scrollValue > 0.85){
+        popup.style.opacity = 1;
+        popup.style.transform = "translateX(0px)";
+    }
+});
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
-
-// Faire apparaitre l'image de la partie improvise
-
-// Faire apparaitre la popup quand on est en bas du site
-
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+closeBtn.addEventListener('click', () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(500px)";
+});
